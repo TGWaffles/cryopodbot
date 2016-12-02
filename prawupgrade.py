6a4e8f8ee7193a5986c151ddac173efb84499954
TGWaffles <thomas.wilcox@hotmail.co.uk>
2016-12-02 16:41:49 -0500

    TONS of changes - new bot features (editing comment with the next part), new discord bot features (try !stats) and more!

--- prawupgrade.py
+++
@@ -1,4 +0,0 @@
-import praw
-from pw_bot import *
-user_agent = ("CryoChecker 1.0")
-r = praw.Reddit(user_agent = user_agent, client_id = CLIENT_ID, client_secret = CLIENT_SECRET, redirect_uri="http://144.217.82.211:65010/authorize_callback")
